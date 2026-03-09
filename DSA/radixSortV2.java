9840c83a568416f5c57851f4152e3552d004c290
Hsu Myat Htike <147356979+hsu-m-h@users.noreply.github.com>
2026-03-09 17:31:04 +0630

    Refactor radix sort to use bubble sort for digits

--- DSA/radixSortV2.java
+++ DSA/radixSortV2.java
@@ -1,64 +1,67 @@
 // This radix sort implementation uses bubble sort as the 
 // sorting algorithm for each digit.
 
-public class radixSortV2{
-    public static void main(String[]args){
-        int my_array[] = {170, 45, 75, 90, 802, 24, 2, 66};
-    
-        int max_val = my_array[0];
-        for(int val : my_array){
-            if(val > max_val){
-                max_val = val;
+public class radixSortV2 {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
+    }
 
-      
-        int radixArray[][] = new int[10][my_array.length];
-        int counts[] = new int[10];
+    public static void radixSortWithBubbleSort(int[] arr) {
+        int maxVal = arr[0];
+        for (int num : arr) {
+            if (num > maxVal) maxVal = num;
+        }
+        
         int exp = 1;
-
-        while(max_val / exp > 0){
+        while (maxVal / exp > 0) {
+            int[][] radixArray = new int[10][arr.length];
+            int[] count = new int[10];
             
-            for(int num : my_array){
-                int radixIndex = (num / exp) %10;
-                radixArray[radixIndex][counts[radixIndex]] = num;
-                counts[radixIndex]++;
+            for (int num : arr) {
+                int radixIndex = (num / exp) % 10;
+                radixArray[radixIndex][count[radixIndex]++] = num;
             }
-
-            int pos = 0;
-            for(int i = 0; i < 10; i++){
-                for(int j = 0; j < counts[i]; j++){
-                    my_array[pos] = radixArray[i][j];
-                    pos++;
+            
+            for (int i = 0; i < 10; i++) {
+                int[] tempArray = new int[count[i]];
+                System.arraycopy(radixArray[i], 0, tempArray, 0, count[i]);
+                bubbleSort(tempArray);
+                System.arraycopy(tempArray, 0, radixArray[i], 0, count[i]);
+            }
+            
+            int index = 0;
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < count[i]; j++) {
+                    arr[index++] = radixArray[i][j];
                 }
-                counts[i] = 0;
             }
+            
             exp *= 10;
         }
+    }
 
-        System.out.println("Sorted Array: ");
-        for(int val : my_array){
-            System.out.print(val + " ");
-        }
+    public static void main(String[] args) {
+        int[] myArray = {170, 45, 75, 90, 802, 24, 2, 66};
+        System.out.print("Original array: ");
+        for (int num : myArray) System.out.print(num + " ");
         System.out.println();
-
         
+        radixSortWithBubbleSort(myArray);
         
+        System.out.print("Sorted array: ");
+        for (int num : myArray) System.out.print(num + " ");
+        System.out.println();
     }
-
-    public static void bubbleSort(int[]my_array){
-        int n = my_array.length;
-                for(int i = 0; i < n - 1; i++){
-            for(int j = 0; j < n - i - 1;j++){
-                if(my_array[j] > my_array[j+1]){
-                    int temp = my_array[j];
-                    my_array[j] = my_array[j+1];
-                    my_array[j+1] = temp;
-                }
-            }
-        }
-
-    }
-
-    
 }
+
+//Java
